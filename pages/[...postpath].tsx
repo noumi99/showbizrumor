c11c1e7d8d6bfd96689140a5eb500d02c922f085
showbizrumor <138694242+showbizrumor@users.noreply.github.com>
2023-07-15 22:14:32 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://showbizrumor.000webhostapp.com/` + encodeURI(path as string)
+					`https://showbizrumor.blogspot.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
